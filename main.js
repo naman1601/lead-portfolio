01dca362eefea1d7ca3a77bc759f6cde91ef095a
naman1601 <namanchhaparia1601@gmail.com>
2021-04-20 18:43:03 +0530

    fixed go to top button

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 //Get the button:
 var mybutton = document.getElementById("backToTop");
+var buttonControl = document.body;
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (buttonControl.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
+
+console.log(buttonControl.scrollTop);
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
